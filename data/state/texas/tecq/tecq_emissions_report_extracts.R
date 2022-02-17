7d31ac5e991888e53665ac85f053096fd8026007
Brian Robinson <barob1n@yahoo.com>
2022-02-17 07:27:26 -0600

    Update tecq_emissions_report_extracts.R

--- data/state/texas/tecq/tecq_emissions_report_extracts.R
+++ data/state/texas/tecq/tecq_emissions_report_extracts.R
@@ -15,22 +15,41 @@ d_n<-unlist(lapply(d,function(x){
   NROW(strsplit(x,'\\|')[[1]])
 }))
 
+## parse type of information (second column)
 d_2<-unlist(lapply(d,function(x){
   strsplit(x,'\\|')[[1]][2]
 }))
 
+## split along type
 d_split<-split(d,d_2)
 
-lapply(d_split$`ACCOUNT-SITE`,function(x){
-  
+## parse site information
+get_site<-function(x){
+  str<-'(E\\|ACCOUNT-SITE\\|RN.*?\\|)(.*?)(\\|)(.*?)(\\|)'
+  tmp<-data.frame(as.list(c(RN,gsub(str,'\\4',x))),stringsAsFactors = F)
+  names(tmp)<-c('RN',gsub(str,'\\2',x))
+  tmp
+}
+d_split$`ACCOUNT-SITE`<-get_site(d_split$`ACCOUNT-SITE`)
+
+
+tmp<-lapply(d_split$`ACCOUNT-SITE`,function(x){
+  strsplit(x,'\\|')[[1]][5]
+})
+data.frame(tmp,stringsAsFactors=F)
+
+## Parse Activity
+d_split$ACTIVITY<-paste0(
+  d_split$ACTIVITY[grepl('FROM DATE',d_split$ACTIVITY)],
+  d_split$ACTIVITY[grepl('TO DATE',d_split$ACTIVITY)])
+
+
+d_split$ACTIVITY<-lapply(d_split$ACTIVITY,function(x){
+  x<-strsplit(x,'\\|')[[1]]
+  data.frame(RN=RN,ACTIVITY=x[3],FROM_DATE=x[5],TO_DATE=x[10],stringsAsFactors = F)
 })
 
-paste0(d_split$ACTIVITY[grepl('FROM DATE',d_split$ACTIVITY)],
-       d_split$ACTIVITY[grepl('TO DATE',d_split$ACTIVITY)])
-
-lapply(d_split$ACTIVITY,function(x){
-  
-})
+d_split$ACTIVITY<-do.call(rbind,d_split$ACTIVITY)
 
 lapply(d_split,function(x){
   x<-strsplit(x,'\\|')[[1]]
