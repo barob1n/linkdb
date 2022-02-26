a6776fe69592308f673380c23d34e94f5fcc5b4d
Brian Robinson <barob1n@yahoo.com>
2022-02-25 18:40:02 -0600

    moved data scripts to separate repo

--- data/state/texas/tecq/state_summary.R
+++
@@ -1,4 +0,0 @@
-setwd("~/GitHub/linkdb/data/state/texas/tecq")
-url<-"https://www.tceq.texas.gov/downloads/air-quality/point-source/2014_2020statesum.xlsx"
-curl::curl_download(url,destfile="state_summary.xlsx")
-
